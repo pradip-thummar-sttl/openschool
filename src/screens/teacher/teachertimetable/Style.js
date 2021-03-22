bd1a617d24677a361a9a030dc4a5aadeba141001
babulapradhan <mobilesttl@Mobiles-Mac-mini.local>
2021-03-22 18:37:01 +0530

    updated for time table UI

--- src/screens/teacher/teachertimetable/Style.js
+++ src/screens/teacher/teachertimetable/Style.js
@@ -14,17 +14,48 @@ export default StyleSheet.create({
         flex: 1,
         flexDirection: 'row'
     },
-    days: {
-        flexDirection: 'column'
-    },
-    day: {
-        width: 200,
-        height: 100
+    backOpacity:{
+        opacity: 0.4,
+        position: 'absolute',
+        top: 0,
+        left:0,
+        width: hp(16),
+        height: hp(8.59),
     },
     lable: {
-        fontSize: 18,
-        padding: 8,
-        fontWeight: 'bold'
+        width: hp(16),
+        height: hp(8.59),
+    },
+    dayRightmain: {
+        width: hp(16),
+        height: hp(8.59),
+        paddingLeft: hp(1.56),
+        paddingTop: hp(1.7),
+    },
+    labledataTitle: {
+        fontSize: hp(1.82),
+        marginBottom: hp(0.3),
+        color: COLORS.darkGray,
+        fontFamily: FONTS.fontRegular,
+    },
+    dayLeft: {
+        width: hp(16),
+        height: hp(8.59),
+        alignItems: 'center',
+        paddingLeft: hp(1),
+        paddingRight: hp(1),
+        paddingTop: hp(1),
+    },
+    lableDay: {
+        fontFamily:FONTS.fontRegular,
+        fontSize:hp(1.82),
+        color:COLORS.darkGray,
+        textAlign: 'center',
+    },
+    labelTime: {
+        fontFamily:FONTS.fontRegular,
+        fontSize:hp(1.82),
+        color:COLORS.darkGray,
     },
     whiteBoard: {
         backgroundColor: COLORS.white,
@@ -35,9 +66,27 @@ export default StyleSheet.create({
         shadowOffset: { width: 0, height: hp(0.2), },
         shadowOpacity: 0.16,
         shadowRadius: hp(1.95),
-        overflow: 'hidden',
-        height: hp(65),
         padding: hp(5),
         marginTop: hp(4),
+    },
+    timeIcon:{
+        width:hp(1.8),
+        resizeMode:'contain',
+        marginRight:hp(1),
+    },
+    row:{
+        flexDirection:'row',
+        alignItems: 'center',
+    },
+    timeLabel:{
+        fontFamily:FONTS.fontRegular,
+        fontSize:hp(1.82),
+        color:COLORS.lightGray,
+    },
+    spaceTop:{
+        marginTop:hp(3.25),
+    },
+    backgroundTable:{
+        backgroundColor:COLORS.backgroundColorCommon,
     },
 });
