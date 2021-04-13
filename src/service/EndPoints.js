4936c46d6d30cc9b1f60109ddab364cefd33b42f
Pradip0344 <69508356+Pradip0344@users.noreply.github.com>
2021-04-13 19:03:27 +0530

    call api get usertype, myday, pupil homework list

--- src/service/EndPoints.js
+++ src/service/EndPoints.js
@@ -1,17 +1,25 @@
 export const EndPoints = {
+    GetAllUserType: "usertype",
     Login: 'login',
+    // Teacher
     GetLessionById: 'getalllesson',
     PupilByTeacherId: 'pupilbyteacherid',
     GetTimeTable: 'gettimetable',
-    CalenderEvent:'calenderevent/',
-    GetSubjectBySchoolId:'subjectbyschoolid/',
-    GetParticipants:'getparticipants/',
-    GetPupilByTeacherId:'pupilbyteacherid/',
-    Homework:"homework",
-    HomeworkUpdate:'homeworkupdate',
+    CalenderEvent: 'calenderevent/',
+    GetSubjectBySchoolId: 'subjectbyschoolid/',
+    GetParticipants: 'getparticipants/',
+    GetPupilByTeacherId: 'pupilbyteacherid/',
+    Homework: "homework",
+    HomeworkUpdate: 'homeworkupdate',
     Lesson: 'lesson',
     LessonUpdate: 'lessonupdate/',
     LessonMaterialUpload: 'lessonmaterialupload/',
     EventType: 'eventtype',
-    HomeworkSubmited: 'homeworksubmited/'
+    HomeworkSubmited: 'homeworksubmited/',
+
+    //Pupil
+    GetListOfPupilMyDay: "getmydaypupil",
+    GetAllHomeworkListByPupil:"allpupilhomeworklist",
+
+
 }
