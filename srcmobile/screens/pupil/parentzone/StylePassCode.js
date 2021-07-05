fcc43cd95fa32d42eda6ae61eef89fc10e0a142c
Proxy_Patrix <pratik.acharya@silvertouch.com>
2021-07-05 18:43:01 +0530

    UI pa changes passcode

--- srcmobile/screens/pupil/parentzone/StylePassCode.js
+++ srcmobile/screens/pupil/parentzone/StylePassCode.js
@@ -24,6 +24,10 @@ export default StyleSheet.create({
         justifyContent: 'center',
         marginTop: hp(10),
     },
+    roundButtonMain: {
+        width: '28%',
+        alignItems: 'center',
+    },
     roundButton: {
         height: hp(8.86),
         width: hp(8.86),
