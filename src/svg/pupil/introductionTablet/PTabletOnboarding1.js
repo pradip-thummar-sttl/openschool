7527a0096a243560639b5bac61fec10211e36264
pradip-thummar-sttl <76721909+pradip-thummar-sttl@users.noreply.github.com>
2021-08-17 16:11:00 +0530

    login and introduction svg convert

---
+++ src/svg/pupil/introductionTablet/PTabletOnboarding1.js
@@ -0,0 +1,488 @@
+import * as React from "react"
+import Svg, { G, Path, Circle, Rect, Ellipse } from "react-native-svg"
+
+function PTabletOnboarding1(props) {
+  return (
+    <Svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={707}
+      height={344.006}
+      viewBox="0 0 707 344.006"
+      {...props}
+    >
+      <G data-name="Group 2196">
+        <G data-name="Group 1765">
+          <Path
+            data-name="Rectangle 17768"
+            d="M365 93h208a57 57 0 0157 57v54a57 57 0 01-57 57H422a57 57 0 01-57-57V93z"
+            fill="#d6f5fe"
+          />
+          <Path
+            data-name="Rectangle 17539"
+            d="M621 56h49a37 37 0 0137 37 37 37 0 01-37 37h-86V93a37 37 0 0137-37z"
+            fill="#dc7eff"
+          />
+          <Path
+            data-name="Path 4026"
+            d="M656.998 94.002a6.019 6.019 0 116 6.5 6.267 6.267 0 01-6-6.5zm-20 0a6.5 6.5 0 116.5 6.5 6.5 6.5 0 01-6.5-6.5zm-19 0a6.5 6.5 0 116.5 6.5 6.5 6.5 0 01-6.5-6.5z"
+            fill="#fff"
+          />
+        </G>
+        <G data-name="Group 1766">
+          <Path
+            data-name="Rectangle 17466"
+            d="M120 16h149a74 74 0 0174 74v98H120a74 74 0 01-74-74V90a74 74 0 0174-74z"
+            fill="#f9fafc"
+            style={{
+              mixBlendMode: "multiply",
+              isolation: "isolate",
+            }}
+          />
+          <Path
+            data-name="Rectangle 17571"
+            d="M120 16h149a74 74 0 0174 74v98H120a74 74 0 01-74-74V90a74 74 0 0174-74z"
+            fill="#d7ff86"
+            style={{
+              mixBlendMode: "multiply",
+              isolation: "isolate",
+            }}
+          />
+          <G data-name="Group 1816">
+            <Path
+              data-name="Rectangle 17540"
+              d="M42.5 0h36A42.5 42.5 0 01121 42.5V85H42.5A42.5 42.5 0 010 42.5 42.5 42.5 0 0142.5 0z"
+              fill="#8ce33a"
+            />
+            <Path
+              data-name="Path 4025"
+              d="M74 42.5a6.019 6.019 0 116 6.5 6.267 6.267 0 01-6-6.5zm-20 0a6.5 6.5 0 116.5 6.5 6.5 6.5 0 01-6.5-6.5zm-19 0a6.5 6.5 0 116.5 6.5 6.5 6.5 0 01-6.5-6.5z"
+              fill="#fff"
+            />
+            <Path
+              data-name="Path 4026"
+              d="M74 42.5a6.019 6.019 0 116 6.5 6.267 6.267 0 01-6-6.5zm-20 0a6.5 6.5 0 116.5 6.5 6.5 6.5 0 01-6.5-6.5zm-19 0a6.5 6.5 0 116.5 6.5 6.5 6.5 0 01-6.5-6.5z"
+              fill="#262626"
+            />
+          </G>
+        </G>
+        <G transform="translate(-133.281 -213.85)">
+          <Circle
+            data-name="Ellipse 521"
+            cx={89}
+            cy={89}
+            r={89}
+            transform="translate(232.281 246.85)"
+            fill="none"
+          />
+          <G data-name="Group 1829" transform="translate(270.11 258.474)">
+            <Path
+              data-name="Path 4048"
+              d="M63.243 16.579A35.856 35.856 0 0199.1 52.435v.066c0 19.8-16.053 20.122-35.856 20.122s-35.857-.319-35.857-20.122v-.066a35.856 35.856 0 0135.856-35.856z"
+              fill="#008658"
+            />
+            <Path
+              data-name="Path 4277"
+              d="M15.209 39.174a17.412 17.412 0 0116.871-17.4 17.412 17.412 0 1133.954-7.655 17.412 17.412 0 0127.483 13.067A14.149 14.149 0 1175.03 45.663a17.417 17.417 0 01-15.988-14.072 17.329 17.329 0 01-9.568 3.222 17.41 17.41 0 11-34.265 4.361z"
+              fill="#008b56"
+            />
+            <Path
+              data-name="Rectangle 17652"
+              fill="#ff8076"
+              d="M46.674 100.196h33.819v31.555H46.674z"
+            />
+            <Path
+              data-name="Path 4490"
+              d="M34.793 83.784v-13.12a11.487 11.487 0 11.476-22.642 29.384 29.384 0 0157.766-.207 11.488 11.488 0 01.515 22.96v13.01z"
+              fill="#ff8076"
+            />
+            <Rect
+              data-name="Rectangle 17600"
+              width={16.789}
+              height={7.953}
+              rx={3.976}
+              transform="rotate(-14.98 165.376 -140.453)"
+              fill="#262626"
+            />
+            <Rect
+              data-name="Rectangle 17601"
+              width={15.952}
+              height={8.836}
+              rx={4.418}
+              transform="rotate(12.04 -118.82 357.346)"
+              fill="#262626"
+            />
+            <Path
+              data-name="Intersection 67"
+              d="M52.267 114.251l8.517 10.175h4.4l8.517-10.175z"
+              fill="#fff"
+            />
+            <Path
+              data-name="Path 3691"
+              d="M60.11 74.543a6.063 6.063 0 006.041 3.03c3.163-.1 4.947-2.034 5.676-1.633s.334 2.33-1.962 3.562a9.055 9.055 0 01-7.722.338 7.247 7.247 0 01-3.867-3.272c-1.046-1.555.343-4.148 1.835-2.025z"
+              fill="#090000"
+            />
+            <G data-name="Group 1824">
+              <G data-name="Group 1822" transform="translate(44.575 46.794)">
+                <Circle
+                  data-name="Ellipse 498"
+                  cx={9.793}
+                  cy={9.793}
+                  r={9.793}
+                  fill="#fff"
+                />
+                <Ellipse
+                  data-name="Ellipse 500"
+                  cx={4.896}
+                  cy={5.441}
+                  rx={4.896}
+                  ry={5.441}
+                  transform="translate(9.793 3.264)"
+                />
+              </G>
+              <G data-name="Group 1823" transform="translate(67.426 46.794)">
+                <Circle
+                  data-name="Ellipse 498"
+                  cx={9.793}
+                  cy={9.793}
+                  r={9.793}
+                  fill="#fff"
+                />
+                <Circle
+                  data-name="Ellipse 500"
+                  cx={5.441}
+                  cy={5.441}
+                  r={5.441}
+                  transform="translate(8.705 3.264)"
+                />
+              </G>
+            </G>
+            <Path
+              data-name="Path 4292"
+              d="M59.742 105.547a24.954 24.954 0 01-24.954-24.951v-9.871h.581v-.785h58.222v8.311h-.048v5.115a22.177 22.177 0 01-22.176 22.182z"
+              fill="#008b56"
+            />
+            <G data-name="Group 1855">
+              <Path
+                data-name="Path 4096"
+                d="M63.356 81.28l3.861.338a6.516 6.516 0 014.564 2.37 10.725 10.725 0 003.403-6.947 2.523 2.523 0 00-2.296-2.734l-16.424-1.434a2.523 2.523 0 00-2.734 2.294 10.757 10.757 0 003.628 9.024 6.176 6.176 0 015.998-2.91z"
+                fill="#262626"
+              />
+              <Path
+                data-name="Path 4097"
+                d="M55.749 82.588c1.568 2.029 2.355 2.695 9.167 3.335s8.315-3.058 8.315-3.058-10.37-7.952-17.482-.277z"
+                fill="#d14034"
+              />
+            </G>
+            <Path
+              data-name="Path 4578"
+              d="M-3.828 143.375c-.328 0-.656 0-.982-.006a40.019 40.019 0 0138.98-30.994h58a40.017 40.017 0 0138.982 31z"
+              fill="#dc7eff"
+            />
+          </G>
+          <G data-name="Group 1857">
+            <Path
+              data-name="Path 4276"
+              d="M342.624 386.14a1.439 1.439 0 01-1.319-.521l-7.6-9.1a.926.926 0 00-1.634 0l-7.6 9.1a1.964 1.964 0 01-2.544.1l-1.508-1.51-11.561-9.133a1.971 1.971 0 01-.216-2.426l4.289-5.207 2.233-3.211H350.6l2.212 3.17 4.319 5.248a1.974 1.974 0 01-.217 2.426l-11.565 9.137-1.5 1.506a1.445 1.445 0 01-1.025.436 1.372 1.372 0 01-.2-.015z"
+              fill="#fff"
+            />
+            <Path
+              data-name="Path 4099"
+              d="M315.553 364.236l17.411 13.329 17.968-13.329h-35.379"
+              fill="#ff8076"
+            />
+          </G>
+          <Ellipse
+            data-name="Ellipse 542"
+            cx={8.5}
+            cy={4.5}
+            rx={8.5}
+            ry={4.5}
+            transform="translate(327.281 322.85)"
+            fill="#ff8076"
+          />
+          <G
+            data-name="Group 1899"
+            transform="translate(300.74 359.881)"
+            fill="#fff"
+          >
+            <Rect
+              data-name="Rectangle 17679"
+              width={15.25}
+              height={32.897}
+              rx={6}
+              transform="rotate(-58 12 6.651)"
+            />
+            <Rect
+              data-name="Rectangle 17680"
+              width={15.25}
+              height={33.593}
+              rx={6}
+              transform="rotate(-122 27.169 5.03)"
+            />
+          </G>
+        </G>
+        <G transform="translate(457 100.814)">
+          <Path
+            data-name="Path 4252"
+            d="M43.188 160.009a52.121 52.121 0 01-8.693-11.282q-.69.358-1.4.68v10.6h-57.849a29.148 29.148 0 01-23.247-11.526v-7.623a40.554 40.554 0 0136.765-40.377v-6.658A31.364 31.364 0 0112.621 63.37a38.929 38.929 0 0177.787 2.336v6.535a51.912 51.912 0 0125.589 87.768z"
+            fill="#8738bd"
+          />
+          <Circle
+            data-name="Ellipse 507"
+            cx={12.001}
+            cy={12.001}
+            transform="translate(6.534 72.281)"
+            fill="#ff8076"
+            r={12.001}
+          />
+          <Path
+            data-name="Path 4047"
+            d="M67.719 131.86l30.131 27.312H8.125l28.286-27.312z"
+            fill="#fff"
+          />
+          <G data-name="Group 1832" transform="translate(13.264 31.474)">
+            <Path
+              data-name="Path 4258"
+              d="M70.951 42.417a12 12 0 11-.215 19.385 31.654 31.654 0 01-16.428 24.022v10.075a15.773 15.773 0 01-31.546 0v-11a31.617 31.617 0 01-15.065-26.95V43.482a31.636 31.636 0 0163.254-1.065z"
+              fill="#ff8076"
+            />
+            <Rect
+              data-name="Rectangle 17600"
+              width={17.54}
+              height={8.309}
+              rx={4.154}
+              transform="rotate(-165.02 31.97 11.912)"
+              fill="#262626"
+            />
+            <Rect
+              data-name="Rectangle 17601"
+              width={16.666}
+              height={9.232}
+              rx={4.616}
+              transform="scale(-1) rotate(-23 -84.774 69.3)"
+              fill="#262626"
+            />
+            <G data-name="Group 1824">
+              <G data-name="Group 1822" transform="translate(35.357 33.39)">
+                <Circle
+                  data-name="Ellipse 498"
+                  cx={10.231}
+                  cy={10.231}
+                  transform="translate(3.409 2.274)"
+                  fill="#fff"
+                  r={10.231}
+                />
+                <Ellipse
+                  data-name="Ellipse 500"
+                  cx={5.116}
+                  cy={5.684}
+                  rx={5.116}
+                  ry={5.684}
+                  transform="translate(3.409 7.86)"
+                  fill="#262626"
+                />
+              </G>
+              <G data-name="Group 1823" transform="translate(11.486 33.39)">
+                <Circle
+                  data-name="Ellipse 498"
+                  cx={10.231}
+                  cy={10.231}
+                  transform="translate(3.407 2.274)"
+                  fill="#fff"
+                  r={10.231}
+                />
+                <Ellipse
+                  data-name="Ellipse 515"
+                  cx={5.116}
+                  cy={5.684}
+                  rx={5.116}
+                  ry={5.684}
+                  transform="translate(3.407 7.859)"
+                  fill="#262626"
+                />
+              </G>
+            </G>
+            <G data-name="Group 1831" transform="translate(0 56.128)">
+              <Circle
+                data-name="Ellipse 514"
+                cx={2.176}
+                cy={2.176}
+                r={2.176}
+                transform="translate(.544)"
+                fill="#fff"
+              />
+              <Rect
+                data-name="Rectangle 17618"
+                width={5.439}
+                height={10.878}
+                rx={2.72}
+                transform="translate(0 9.857)"
+                fill="#ffd719"
+              />
+            </G>
+            <G data-name="Group 1833" transform="translate(75.059 56.128)">
+              <Circle
+                data-name="Ellipse 514"
+                cx={2.176}
+                cy={2.176}
+                r={2.176}
+                transform="translate(.544)"
+                fill="#fff"
+              />
+              <Rect
+                data-name="Rectangle 17619"
+                width={5.439}
+                height={10.878}
+                rx={2.72}
+                transform="translate(0 9.857)"
+                fill="#ffd719"
+              />
+            </G>
+            <Path
+              data-name="Path 3691"
+              d="M42.998 61.813c-1.558 2.214-3.01 3.275-6.313 3.168s-5.167-2.127-5.933-1.71-.348 2.433 2.05 3.72a9.46 9.46 0 008.068.353 7.572 7.572 0 004.042-3.423c1.094-1.618-.356-4.326-1.914-2.108z"
+              fill="#262626"
+            />
+          </G>
+          <Path
+            data-name="Path 4889"
+            d="M-28.983 160.007q-.5 0-1-.018a50 50 0 0145.3-28.809h69.7a50 50 0 0145.308 28.827z"
+            fill="#8ce33a"
+          />
+          <Path
+            data-name="Rectangle 17764"
+            d="M36 130.186h32v9a11 11 0 01-11 11H47a11 11 0 01-11-11v-9z"
+            fill="#00b754"
+          />
+          <Ellipse
+            data-name="Ellipse 668"
+            cx={31.317}
+            cy={18.662}
+            rx={31.317}
+            ry={18.662}
+            transform="rotate(-36 71.581 23.77)"
+            fill="#8738bd"
+          />
+        </G>
+        <Path
+          data-name="Rectangle 17538"
+          d="M194 211h149v76a57 57 0 01-57 57h-92a57 57 0 01-57-57v-19a57 57 0 0157-57z"
+          fill="#ffd719"
+        />
+        <Path
+          data-name="Rectangle 17761"
+          d="M217 265h50a14 14 0 0114 14v26a14 14 0 01-14 14h-36a14 14 0 01-14-14v-40z"
+          fill="#262626"
+        />
+        <G data-name="Group 1829" transform="translate(-349.035 -32.752)">
+          <Ellipse
+            data-name="Ellipse 670"
+            cx={24}
+            cy={21.5}
+            rx={24}
+            ry={21.5}
+            transform="translate(563.035 276.752)"
+            fill="#262626"
+          />
+          <Circle
+            data-name="Ellipse 507"
+            cx={10.557}
+            cy={10.557}
+            r={10.557}
+            transform="translate(617.906 299.689)"
+            fill="#d14034"
+          />
+          <Path
+            data-name="Union 51"
+            d="M585.12 372.067v-30.1a27.559 27.559 0 01-13.156-16.788 10.556 10.556 0 11-1.277-16.117 27.829 27.829 0 0155.642.937v5.319a28.012 28.012 0 01-.417 4.825 27.521 27.521 0 01-17.006 23.254v28.67z"
+            fill="#d06444"
+          />
+          <Path
+            data-name="Rectangle 17765"
+            fill="#d06444"
+            d="M588.035 296.752h38v17h-38z"
+          />
+          <Path
+            data-name="Path 4047"
+            d="M585.256 354.049l-22.678 22.7h67.536l-21.29-22.7z"
+            fill="#90daff"
+          />
+          <Path
+            data-name="Path 4259"
+            d="M588.069 376.75l7.827-22.7h4.044l7.827 22.7z"
+            fill="#8738bd"
+          />
+          <Path
+            data-name="Path 4413"
+            d="M598.655 268.257h.719c15.28 0 26.726 12.477 26.726 27.934v3.823s-23.69 4.113-40.406-14.714c-.462-.4-15.071 23.7-15.071 24.181l.365-13.232a27.83 27.83 0 0127.667-27.992z"
+            fill="#262626"
+          />
+          <Rect
+            data-name="Rectangle 17600"
+            width={15.43}
+            height={7.309}
+            rx={3.654}
+            transform="rotate(-14.98 1411.029 -2050.425)"
+            fill="#262626"
+          />
+          <Path
+            data-name="Path 3691"
+            d="M594.242 324.627a5.572 5.572 0 005.552 2.788c2.906-.095 4.545-1.871 5.216-1.503s.306 2.14-1.804 3.273a8.322 8.322 0 01-7.097.31 6.661 6.661 0 01-3.556-3.011c-.96-1.424.318-3.808 1.69-1.857z"
+            fill="#262626"
+          />
+          <G data-name="Group 1824">
+            <G data-name="Group 1822" transform="translate(578 300.803)">
+              <Circle
+                data-name="Ellipse 498"
+                cx={9}
+                cy={9}
+                r={9}
+                transform="translate(3 2)"
+                fill="#fff"
+              />
+              <Ellipse
+                data-name="Ellipse 500"
+                cx={4.5}
+                cy={5}
+                rx={4.5}
+                ry={5}
+                transform="translate(11 4)"
+                fill="#262626"
+              />
+            </G>
+            <G data-name="Group 1823" transform="translate(599.75 300.803)">
+              <Circle
+                data-name="Ellipse 498"
+                cx={9}
+                cy={9}
+                r={9}
+                transform="translate(2.25 2)"
+                fill="#fff"
+              />
+              <Circle
+                data-name="Ellipse 500"
+                cx={5}
+                cy={5}
+                r={5}
+                transform="translate(9.25 4)"
+                fill="#262626"
+              />
+            </G>
+          </G>
+          <Path
+            data-name="Subtraction 41"
+            d="M635.035 376.758h-91.887a43.938 43.938 0 016.922-7.672 55.632 55.632 0 019.057-6.461 68.506 68.506 0 0110.855-5.021 79.235 79.235 0 0112.324-3.354h32.455a74.358 74.358 0 0121.564 7.475 58.274 58.274 0 018.691 5.7 47.121 47.121 0 016.953 6.771 56.9 56.9 0 01-16.934 2.562z"
+            fill="#90daff"
+          />
+          <Path
+            data-name="Rectangle 17759"
+            d="M585.035 353.752h24v4a11 11 0 01-11 11h-2a11 11 0 01-11-11v-4z"
+            fill="#cdf47f"
+          />
+        </G>
+      </G>
+    </Svg>
+  )
+}
+
+export default PTabletOnboarding1
