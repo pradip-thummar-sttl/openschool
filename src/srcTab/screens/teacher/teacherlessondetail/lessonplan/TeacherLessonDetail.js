cc3d707035ae7b53a0d50393af989a053b6d7316
Proxy_Patrix <pratik.acharya@silvertouch.com>
2021-09-02 11:20:17 +0530

    UI pa chagne

--- src/srcTab/screens/teacher/teacherlessondetail/lessonplan/TeacherLessonDetail.js
+++ src/srcTab/screens/teacher/teacherlessondetail/lessonplan/TeacherLessonDetail.js
@@ -140,103 +140,105 @@ const TLDetail = (props) => {
                         </View>
                     </View>
                 </View>
-                {props.lessonData.ChatTranscript.length > 0 || props.lessonData.RecommendedList.length > 0 || props.lessonData.MaterialList.length > 0 ?
-                    <View style={PAGESTYLE.rightSideBar}>
-                        {props.lessonData.MaterialList.length > 0 ?
-                            <View style={PAGESTYLE.fileBoxGrpWrap}>
-                                <Text style={PAGESTYLE.requireText}>Learning material</Text>
+                <View style={PAGESTYLE.rightSideBar}>
+                    {props.lessonData.ChatTranscript.length > 0 || props.lessonData.RecommendedList.length > 0 || props.lessonData.MaterialList.length > 0 ?
+                        <>
+                            {props.lessonData.MaterialList.length > 0 ?
+                                <View style={PAGESTYLE.fileBoxGrpWrap}>
+                                    <Text style={PAGESTYLE.requireText}>Learning material</Text>
 
-                                {props.lessonData.MaterialList.map((item, index) => {
-                                    return (
-                                        <TouchableOpacity onPress={() => {
-                                            setLoader(true);setMateIndex(index); Download(item, (res) => {
-                                                setLoader(false)
-                                                setMateIndex(-1)
-                                            })
-                                        }} style={{ ...PAGESTYLE.fileGrp, height: 60 }}>
-                                            <Text numberOfLines={1} style={[PAGESTYLE.fileName, { width: hp(20) }]}>{item.originalname}</Text>
-                                            <View style={PAGESTYLE.downloaBtn}>
-                                                {(isMatLoading && index==mateIndex) ?
-                                                    <ActivityIndicator
-                                                        style={{ ...PAGESTYLE.downloadIcon }}
-                                                        size={Platform.OS == 'ios' ? 'large' : 'small'}
-                                                        color={COLORS.white} />
-                                                    :
-                                                    // <Image source={Images.Download} style={PAGESTYLE.downloadIcon} />
-                                                    <DownloadSVG style={PAGESTYLE.downloadIcon} height={hp(2.01)} width={hp(2.01)} />
-                                                }
-                                            </View>
-                                        </TouchableOpacity>
-                                    )
-                                })}
-                            </View>
-                            :
-                            null
-                        }
-
-                        {props.lessonData.RecommendedList.length > 0 ?
-                            <FlatList
-                                data={props.lessonData.RecommendedList}
-                                style={{ alignSelf: 'center', width: '100%', bottom: hp(2.60), marginTop: hp(1.30) }}
-                                renderItem={({ item, index }) => (
-                                    <View style={PAGESTYLE.thumbVideo}>
-                                        {/* <Image source={Images.VideoUpload} style={PAGESTYLE.grpThumbVideo} /> */}
-                                        <DownloadSVG style={PAGESTYLE.grpThumbVideo} height={hp(2.01)} width={hp(2.01)} />
-                                    </View>
-                                )}
-                                keyExtractor={(item, index) => index.toString()}
-                            />
-                            :
-                            null
-                        }
-
-                        {props.lessonData.RecordingList ?
-                            <View style={[PAGESTYLE.videoLinkBlockSpaceBottom, PAGESTYLE.videoLinkBlockSpaceTop]}>
-                                <Text style={PAGESTYLE.requireText}>View lesson recording</Text>
-                                {props.lessonData.RecordingList.map((item, index) => {
-                                    return (
-                                        <TouchableOpacity onPress={() => {
-                                            setRecordLoader(true); Download(item, (res) => {
-                                                setRecordLoader(false)
-                                            })
-                                        }} style={{ ...PAGESTYLE.fileGrp, height: 60 }}>
-                                            <Text numberOfLines={1} style={[PAGESTYLE.fileName, { width: hp(20) }]}>{item.originalname}</Text>
-                                            <View style={PAGESTYLE.downloaBtn}>
-                                                {isRecordLoading ?
-                                                    <ActivityIndicator
-                                                        style={{ ...PAGESTYLE.downloadIcon }}
-                                                        size={Platform.OS == 'ios' ? 'large' : 'small'}
-                                                        color={COLORS.blueBorder} />
-                                                    :
-                                                    // <Image source={Images.Download} style={PAGESTYLE.downloadIcon} />
-                                                    <DownloadSVG style={PAGESTYLE.downloadIcon} height={hp(2.01)} width={hp(2.01)} />
-                                                }
-
-                                            </View>
-                                        </TouchableOpacity>
-                                    )
-                                })}
-                            </View>
-                            :
-                            null
-                        }
-
-                        {props.lessonData.ChatTranscript ?
-                            <View style={PAGESTYLE.fileBoxGrpWrap}>
-                                <Text style={PAGESTYLE.requireText}>Chat transcript</Text>
-                                <View style={PAGESTYLE.fileGrp}>
-                                    <Text style={PAGESTYLE.fileName}>Filename</Text>
-                                    {/* <Image source={Images.Download} style={PAGESTYLE.downloadIcon} /> */}
-                                    <DownloadSVG style={PAGESTYLE.downloadIcon} height={hp(2.01)} width={hp(2.01)} />
+                                    {props.lessonData.MaterialList.map((item, index) => {
+                                        return (
+                                            <TouchableOpacity onPress={() => {
+                                                setLoader(true); setMateIndex(index); Download(item, (res) => {
+                                                    setLoader(false)
+                                                    setMateIndex(-1)
+                                                })
+                                            }} style={{ ...PAGESTYLE.fileGrp, height: 60 }}>
+                                                <Text numberOfLines={1} style={[PAGESTYLE.fileName, { width: hp(20) }]}>{item.originalname}</Text>
+                                                <View style={PAGESTYLE.downloaBtn}>
+                                                    {(isMatLoading && index == mateIndex) ?
+                                                        <ActivityIndicator
+                                                            style={{ ...PAGESTYLE.downloadIcon }}
+                                                            size={Platform.OS == 'ios' ? 'large' : 'small'}
+                                                            color={COLORS.white} />
+                                                        :
+                                                        // <Image source={Images.Download} style={PAGESTYLE.downloadIcon} />
+                                                        <DownloadSVG style={PAGESTYLE.downloadIcon} height={hp(2.01)} width={hp(2.01)} />
+                                                    }
+                                                </View>
+                                            </TouchableOpacity>
+                                        )
+                                    })}
                                 </View>
-                            </View>
-                            :
-                            null
-                        }
-                    </View>
-                    :
-                    null
-                }
+                                :
+                                null
+                            }
+
+                            {props.lessonData.RecommendedList.length > 0 ?
+                                <FlatList
+                                    data={props.lessonData.RecommendedList}
+                                    style={{ alignSelf: 'center', width: '100%', bottom: hp(2.60), marginTop: hp(1.30) }}
+                                    renderItem={({ item, index }) => (
+                                        <View style={PAGESTYLE.thumbVideo}>
+                                            {/* <Image source={Images.VideoUpload} style={PAGESTYLE.grpThumbVideo} /> */}
+                                            <DownloadSVG style={PAGESTYLE.grpThumbVideo} height={hp(2.01)} width={hp(2.01)} />
+                                        </View>
+                                    )}
+                                    keyExtractor={(item, index) => index.toString()}
+                                />
+                                :
+                                null
+                            }
+
+                            {props.lessonData.RecordingList ?
+                                <View style={[PAGESTYLE.videoLinkBlockSpaceBottom, PAGESTYLE.videoLinkBlockSpaceTop]}>
+                                    <Text style={PAGESTYLE.requireText}>View lesson recording</Text>
+                                    {props.lessonData.RecordingList.map((item, index) => {
+                                        return (
+                                            <TouchableOpacity onPress={() => {
+                                                setRecordLoader(true); Download(item, (res) => {
+                                                    setRecordLoader(false)
+                                                })
+                                            }} style={{ ...PAGESTYLE.fileGrp, height: 60 }}>
+                                                <Text numberOfLines={1} style={[PAGESTYLE.fileName, { width: hp(20) }]}>{item.originalname}</Text>
+                                                <View style={PAGESTYLE.downloaBtn}>
+                                                    {isRecordLoading ?
+                                                        <ActivityIndicator
+                                                            style={{ ...PAGESTYLE.downloadIcon }}
+                                                            size={Platform.OS == 'ios' ? 'large' : 'small'}
+                                                            color={COLORS.blueBorder} />
+                                                        :
+                                                        // <Image source={Images.Download} style={PAGESTYLE.downloadIcon} />
+                                                        <DownloadSVG style={PAGESTYLE.downloadIcon} height={hp(2.01)} width={hp(2.01)} />
+                                                    }
+
+                                                </View>
+                                            </TouchableOpacity>
+                                        )
+                                    })}
+                                </View>
+                                :
+                                null
+                            }
+
+                            {props.lessonData.ChatTranscript ?
+                                <View style={PAGESTYLE.fileBoxGrpWrap}>
+                                    <Text style={PAGESTYLE.requireText}>Chat transcript</Text>
+                                    <View style={PAGESTYLE.fileGrp}>
+                                        <Text style={PAGESTYLE.fileName}>Filename</Text>
+                                        {/* <Image source={Images.Download} style={PAGESTYLE.downloadIcon} /> */}
+                                        <DownloadSVG style={PAGESTYLE.downloadIcon} height={hp(2.01)} width={hp(2.01)} />
+                                    </View>
+                                </View>
+                                :
+                                null
+                            }
+                        </>
+                        :
+                        null
+                    }
+                </View>
             </View>
         </View>
 
