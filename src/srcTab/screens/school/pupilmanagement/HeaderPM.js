cf53b790ec1ee7f8a05bacec587d447fc7dda0b5
bhavin <bhavin.pethani@ekzero.com>
2021-12-21 17:56:50 +0530

    hide Teacher Insights in mobile for school

--- src/srcTab/screens/school/pupilmanagement/HeaderPM.js
+++ src/srcTab/screens/school/pupilmanagement/HeaderPM.js
@@ -57,7 +57,8 @@ const HeaderPM = (props) => {
 
                 <View style={styles.headerRight}>
                     <TouchableOpacity style={styles.notificationBar}
-                        onPress={() => null}
+                        // onPress={() => null}
+                        onPress={() => props.onAlertPress()}
                         activeOpacity={opacity}>
                         {/* <Image style={styles.massagesIcon} source={Images.Notification} /> */}
                         <Notification style={styles.massagesIcon} height={hp(5.20)} width={hp(5.20)} />
