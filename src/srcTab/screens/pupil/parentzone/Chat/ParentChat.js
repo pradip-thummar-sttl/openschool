b03ce99bd623267ac0a275bd1a7dec93066e6bee
pateldhruvsttl <77956512+pateldhruvsttl@users.noreply.github.com>
2022-01-24 16:44:40 +0530

    Set the Parentzone issue

--- src/srcTab/screens/pupil/parentzone/Chat/ParentChat.js
+++ src/srcTab/screens/pupil/parentzone/Chat/ParentChat.js
@@ -51,7 +51,7 @@ const ParentChat = (props) => {
         console.log('message', message);
         if (typeof message === 'string' || message.hasOwnProperty('text')) {
             // mesage.push(event)
-            addMessage(messages => [...messages, event]);
+            addMessage(messages => [...messages, event].reverse());
         }
     };
 
@@ -96,7 +96,7 @@ const ParentChat = (props) => {
 
     return (
 
-        <View style={{ flex: 1 }}>
+        <View style={{ height: '100%', }}>
             {/* <ChatHeader /> */}
             {/* tabs */}
             {/* <View style={{ flexDirection: 'row', justifyContent: 'space-between', backgroundColor: 'white' }}>
@@ -124,89 +124,92 @@ const ParentChat = (props) => {
                 </View>
             </View> */}
 
-            {!isLoading && teacherData.length > 0 && selectedTeacherIndex != -1 ?
-                <>
-                    <View style={Styles.views}>
-                        <View style={Styles.leftView}>
-                            <View style={Styles.secondView}>
-                                <Text style={Styles.headText}>Teacher's name:</Text>
-                                <Text style={Styles.subText}>{teacherData[selectedTeacherIndex].TeacherFirstName} {teacherData[selectedTeacherIndex].TeacherLastName}</Text>
-                            </View>
-                            <View style={Styles.secondView}>
-                                <Text style={Styles.headText}>Address:</Text>
-                                <Text style={Styles.subText}>{teacherData[selectedTeacherIndex].TeacherAddressLine1 || teacherData[selectedTeacherIndex].TeacherAddressLine2 ? `${teacherData[selectedTeacherIndex].TeacherAddressLine1}\n${teacherData[selectedTeacherIndex].TeacherAddressLine2}` : '-'}</Text>
-                            </View>
-                            <View style={Styles.secondView}>
-                                <Text style={Styles.headText}>Telephone no:</Text>
-                                <Text style={Styles.subText}>{teacherData[selectedTeacherIndex].TeacherMobileNumber ? teacherData[selectedTeacherIndex].TeacherMobileNumber : '-'}</Text>
-                            </View>
-
-                        </View>
-
-                        <View style={[Styles.rightView, { width: hp(86) }]}>
-                            <View style={{ flexDirection: 'row', width: '100%',}}>
-                                <Text style={Styles.teachers}>Chat with:</Text>
-                                {teacherData.map((item, index) => (
-                                    <TouchableOpacity
-                                        activeOpacity={opacity}
-                                        onPress={() => setSelectedTeacherIndex(index)}>
-                                        <View style={{ ...Styles.checkBoxLabelNone }}>
-                                            <Image source={{ uri: baseUrl + item.ProfilePicture }} style={Styles.userIconPupil} />
-                                            <Text style={{ ...Styles.teachers, fontFamily: selectedTeacherIndex == index ? FONTS.fontSemiBold : FONTS.fontRegular }}>{item.TeacherFirstName} {item.TeacherLastName}</Text>
-                                        </View>
-                                    </TouchableOpacity>
-                                ))}
-                            </View>
-                            <KeyboardAwareScrollView enableOnAndroid={true}
-                                extraScrollHeight={90}
-                                scrollEnabled
-                                enableAutomaticScroll={(Platform.OS === 'ios')} >
-
-
-
-                                <View style={[Styles.mesagesView,{ width: hp(76),  }]}>
-                                    <FlatList
-                                        data={messages}
-                                        showsVerticalScrollIndicator={false}
-                                        renderItem={({ item, index }) => {
-                                            return (
-                                                <View style={Styles.messageCell}>
-                                                    <Image style={Styles.roundImage} source={{ uri: baseUrl + item.message.split('#@#')[2] }} />
-                                                    <View style={Styles.messageSubCell}>
-                                                        <Text style={Styles.userNameText}>{item.message.split('#@#')[1]}<Text style={Styles.timeText}>   {moment(new Date(((item.timetoken / 10000000) * 1000))).format('HH:mm')}</Text></Text>
-                                                        <Text style={Styles.messageText}>{item.message.split('#@#')[0]}</Text>
-                                                    </View>
-                                                </View>
-                                            )
-                                        }} />
+            <View style={{ flex: 1 }}>
+                {!isLoading && teacherData.length > 0 && selectedTeacherIndex != -1 ?
+                    <>
+                        <View style={Styles.views}>
+                            <View style={Styles.leftView}>
+                                <View style={Styles.secondView}>
+                                    <Text style={Styles.headText}>Teacher's name:</Text>
+                                    <Text style={Styles.subText}>{teacherData[selectedTeacherIndex].TeacherFirstName} {teacherData[selectedTeacherIndex].TeacherLastName}</Text>
                                 </View>
-                                <View style={[Styles.textView, { width: hp(76),  }]}>
-                                    <TextInput
-                                        style={Styles.input}
-                                        multiline={true}
-                                        placeholder={placeholder}
-                                        placeholderTextColor={COLORS.menuLightFonts}
-                                        value={message}
-                                        onChangeText={(text) => setMessage(text)}
-                                    />
-                                    <View style={Styles.buttonView}>
-                                        <TouchableOpacity onPress={() => sendMessage(message)}>
-                                            {/* <Image style={Styles./btn} source={Images.send} /> */}
-                                            <Ic_Send style={Styles.btn} height={hp(2.5)} width={hp(2.5)} />
+                                <View style={Styles.secondView}>
+                                    <Text style={Styles.headText}>Address:</Text>
+                                    <Text style={Styles.subText}>{teacherData[selectedTeacherIndex].TeacherAddressLine1 || teacherData[selectedTeacherIndex].TeacherAddressLine2 ? `${teacherData[selectedTeacherIndex].TeacherAddressLine1}\n${teacherData[selectedTeacherIndex].TeacherAddressLine2}` : '-'}</Text>
+                                </View>
+                                <View style={Styles.secondView}>
+                                    <Text style={Styles.headText}>Telephone no:</Text>
+                                    <Text style={Styles.subText}>{teacherData[selectedTeacherIndex].TeacherMobileNumber ? teacherData[selectedTeacherIndex].TeacherMobileNumber : '-'}</Text>
+                                </View>
+
+                            </View>
+
+                            <View style={[Styles.rightView]}>
+                                <View style={{ flexDirection: 'row', width: '100%', }}>
+                                    <Text style={Styles.teachers}>Chat with:</Text>
+                                    {teacherData.map((item, index) => (
+                                        <TouchableOpacity
+                                            activeOpacity={opacity}
+                                            onPress={() => setSelectedTeacherIndex(index)}>
+                                            <View style={{ ...Styles.checkBoxLabelNone }}>
+                                                <Image source={{ uri: baseUrl + item.ProfilePicture }} style={Styles.userIconPupil} />
+                                                <Text style={{ ...Styles.teachers, fontFamily: selectedTeacherIndex == index ? FONTS.fontSemiBold : FONTS.fontRegular }}>{item.TeacherFirstName} {item.TeacherLastName}</Text>
+                                            </View>
                                         </TouchableOpacity>
-                                    </View>
+                                    ))}
                                 </View>
-                            </KeyboardAwareScrollView>
+                                <KeyboardAwareScrollView enableOnAndroid={true}
+                                    extraScrollHeight={90}
+                                    scrollEnabled
+                                    contentContainerStyle={{ flex: 1 }}
+                                    enableAutomaticScroll={(Platform.OS === 'ios')} >
 
+                                    <View style={[Styles.mesagesView, { width: '100%', }]}>
+                                        <FlatList
+                                            data={messages}
+                                            showsVerticalScrollIndicator={false}
+                                            inverted={true}
+                                            style={{ marginBottom: 120, marginHorizontal: 15 }}
+                                            renderItem={({ item, index }) => {
+                                                return (
+                                                    <View style={Styles.messageCell}>
+                                                        <Image style={Styles.roundImage} source={{ uri: baseUrl + item.message.split('#@#')[2] }} />
+                                                        <View style={Styles.messageSubCell}>
+                                                            <Text style={Styles.userNameText}>{item.message.split('#@#')[1]}<Text style={Styles.timeText}>   {moment(new Date(((item.timetoken / 10000000) * 1000))).format('HH:mm')}</Text></Text>
+                                                            <Text style={Styles.messageText}>{item.message.split('#@#')[0]}</Text>
+                                                        </View>
+                                                    </View>
+                                                )
+                                            }} />
+                                    </View>
+                                    <View style={[Styles.textView]}>
+                                        <TextInput
+                                            style={Styles.input}
+                                            multiline={true}
+                                            placeholder={placeholder}
+                                            placeholderTextColor={COLORS.menuLightFonts}
+                                            value={message}
+                                            onChangeText={(text) => setMessage(text)}
+                                        />
+                                        <View style={Styles.buttonView}>
+                                            <TouchableOpacity onPress={() => sendMessage(message)}>
+                                                {/* <Image style={Styles./btn} source={Images.send} /> */}
+                                                <Ic_Send style={Styles.btn} height={hp(2.5)} width={hp(2.5)} />
+                                            </TouchableOpacity>
+                                        </View>
+                                    </View>
+                                </KeyboardAwareScrollView>
+
+                            </View>
                         </View>
-                    </View>
-                </>
-                :
-                <ActivityIndicator
-                    style={{ margin: 20 }}
-                    size={Platform.OS == 'ios' ? 'large' : 'small'}
-                    color={COLORS.yellowDark} />
-            }
+                    </>
+                    :
+                    <ActivityIndicator
+                        style={{ margin: 20 }}
+                        size={Platform.OS == 'ios' ? 'large' : 'small'}
+                        color={COLORS.yellowDark} />
+                }
+            </View>
         </View>
 
     )
