7bd5443ba6cec7770f6f265606541d47a4056de2
Jalpa-SSTL <93371165+Jalpa-SSTL@users.noreply.github.com>
2022-02-21 18:23:46 +0530

    Update network_security_config.xml

--- src/utils/Constant.js
+++ src/utils/Constant.js
@@ -1,5 +1,6 @@
-export const baseUrl = "http://14.143.90.233:3000/"; //public
+// export const baseUrl = "http://14.143.90.233:3000/"; //public
 // export const baseUrl = "http://192.168.0.218:3000/"; //internal
+export const baseUrl = "http://138.68.152.151:3000/"; //live
 import { Alert, Platform } from "react-native";
 
 export const opacity = 0.5;
