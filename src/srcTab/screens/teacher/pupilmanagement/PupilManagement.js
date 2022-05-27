a8708068f764de9d477251839a21e66672157afb
pradip-thummar-sttl <76721909+pradip-thummar-sttl@users.noreply.github.com>
2022-05-27 12:00:46 +0530

    add new pupil implementation in teacher pupil management

--- src/srcTab/screens/teacher/pupilmanagement/PupilManagement.js
+++ src/srcTab/screens/teacher/pupilmanagement/PupilManagement.js
@@ -1,15 +1,25 @@
 import React, { useState, useEffect } from "react";
 import { View } from "react-native";
+import { Var } from "../../../../utils/Constant";
+import { BadgeIcon } from "../../../../utils/Model";
 import PupiloverView from "./PupiloverView";
 import PupilProfileView from "./PupilProfileView";
 import PAGESTYLE from './Style';
+import TPupilProfileAdd from "./TPupilProfileAdd";
 
 
 const PupilManagement = (props) => {
     const [isOverViewSelected, setOverViewSelected] = useState(true);
     const [isDetailSelected, setDetailSelected] = useState(true);
+    const [isNewPupilAdd, setNewPupilAdd] = useState(true);
     const [selectedPupil, setSelectedPupil] = useState({})
 
+    const openNotification = () => {
+        Var.isCalender = false
+        BadgeIcon.isBadge = false
+        props.navigation.openDrawer()
+        // props.navigation.navigate('NotificationDrawer',{ onGoBack: () => {} })
+    }
 
 
     return (
@@ -17,16 +27,18 @@ const PupilManagement = (props) => {
             {isOverViewSelected ?
                 <PupiloverView
                     navigation={props.navigation}
-                    navigateToAddNewUser={() => props.navigation.replace('PupilRegister', { userType: "Pupil" })}
-                    onPupilClick={(item) => { setSelectedPupil(item); setOverViewSelected(false); setDetailSelected(true) }} tabs={props.tabs} />
+                    navigateToAddNewUser={() =>{setNewPupilAdd(true);setOverViewSelected(false); setDetailSelected(false)}}
+                    onPupilClick={(item) => { setSelectedPupil(item); setOverViewSelected(false); setDetailSelected(true);setNewPupilAdd(false); }} tabs={props.tabs} />
                 :
                 isDetailSelected ?
                     <PupilProfileView
                         selectedPupil={selectedPupil}
-                        navigateToBack={() => { setOverViewSelected(true); setDetailSelected(false) }}
+                        navigateToBack={() => { setOverViewSelected(true); setDetailSelected(false);setNewPupilAdd(false); }}
                         navigations={props}
                         />
                     :
+                    isNewPupilAdd?
+                    <TPupilProfileAdd navigateToBack={() => {setOverViewSelected(true); setDetailSelected(false); setNewPupilAdd(false); }} openNotification={() => { openNotification() }}/>:
                     null
             }
         </View>
